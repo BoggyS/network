456a38dc3686ef1c7d2c5b5ba03a56e17092314b
BaltazarSiren <boguslavchik@gmail.com>
2017-12-01 13:38:56 +0100

    first commit

---
+++ RUN.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#<x> <y> <numberOfInputs> <numberOfOutputs> <pathToTest> <pathToExpectations> <numberOfLearningIterations> <learningRate> <visualisation(monitor required)>
+java -jar network.jar 3 784 784 10 mnist_test_vectors.csv mnist_test_labels.csv 10000 0.5 true
