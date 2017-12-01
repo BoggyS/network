1a415f981f9721735fca18a4a053a7547fce6b01
BaltazarSiren <boguslavchik@gmail.com>
2017-12-01 23:26:45 +0100

    Last

--- ZIP/RUN.sh
+++ ZIP/RUN.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #<x> <y> <numberOfInputs> <numberOfOutputs> <pathToTest> <pathToExpectations> <numberOfLearningIterations> <learningRate> <visualisation(monitor required)>
-java -jar newNetwork.jar 3 784 784 10 10000 0.5 mnist_test_vectors.csv mnist_test_labels.csv
+java -jar network.jar 4 5 10 10 100000 0.5 1 mnist_test_vectors.csv mnist_test_labels.csv
